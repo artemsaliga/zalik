a341f93fd7b9dd51e715d8c5034ed2a6d680682d
artemsaliga <145282713+artemsaliga@users.noreply.github.com>
2023-12-08 14:20:06 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+def analyze_text(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            for line_number, line in enumerate(file, start=1):
+                words = len(line.split())
+                characters = len(line)
+                print(f"Рядок {line_number}: Слів - {words}, Символів - {characters}")
+
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+    except Exception as e:
+        print(f"Помилка: {e}")
+
+
+file_path = 'file.txt'
+analyze_text(file_path)
